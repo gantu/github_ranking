38755e69a2ab83115c9319d69865613be888c93d
Mirlan Ipasov <mipasov@gmail.com>
2019-11-30 16:42:52 +0600

    add  model for error handling, not yet implemented

--- src/main/scala/kg/challange/ranking/models/Error.scala
+++ src/main/scala/kg/challange/ranking/models/Error.scala
@@ -1,5 +1,12 @@
 package kg.challange.ranking.models
 
+import cats.Applicative
+import cats.effect.Sync
+import org.http4s.{EntityDecoder, EntityEncoder, Method, Uri, Request}
+import io.circe.{Encoder, Decoder, Json, HCursor}
+import io.circe.generic.semiauto._
+import org.http4s.circe._
+
 sealed trait Error
 
 case object UnauthorizedError extends Error
@@ -8,3 +15,16 @@ case object UnprocessableEntityError extends Error
 case class UnrecognizedError(statusCode: Int) extends Error
 
 final case class GeneralError(e: Throwable) extends RuntimeException
+
+
+case class ServiceResponseError(code: Int, message: String)
+
+object ServiceResponseError {
+    implicit val responseErrorDecoder: Decoder[ServiceResponseError] = deriveDecoder[ServiceResponseError]
+    implicit def responseErrorEntityDecoder[F[_]: Sync]: EntityDecoder[F, ServiceResponseError] =
+        jsonOf[F, ServiceResponseError]
+
+  implicit val responseErrorEncoder: Encoder[ServiceResponseError] = deriveEncoder[ServiceResponseError]
+  implicit def responseErrorEntityEncoder[F[_]: Applicative]: EntityEncoder[F, ServiceResponseError] =
+    jsonEncoderOf[F, ServiceResponseError]
+}
